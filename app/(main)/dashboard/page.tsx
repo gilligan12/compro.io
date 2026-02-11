26798e8e0b5a1cef3d68c8b7ac035016e3d1162b
gilligan12 <patrick.antall2@gmail.com>
2026-02-11 00:43:45 -0500

    Fix profile type assertion - cast newProfile through unknown first

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -43,7 +43,7 @@ export default async function DashboardPage() {
       redirect('/')
     }
     
-    profileData = newProfile
+    profileData = newProfile as Profile
   }
 
   const profile = profileData as Profile
